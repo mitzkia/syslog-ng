b80f4da7f209c947af27b67cc64fbfb10fca4637
Andras Mitzki <andras.mitzki@balabit.com>
2017-12-03 14:00:00 +0100

    Conftest: Detect syslog-ng config version from binary help
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- tests/functional_tests/conftest.py
+++ tests/functional_tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 
+import os
+import shlex
+import subprocess
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", help="Also run @slow tests.")
@@ -7,7 +10,7 @@ def pytest_addoption(parser):
 
     parser.addoption("--installmode", action="store", default="custom", help="Set installmode of syslog-ng. Available modes: ['path', 'osepkg', 'balabitpkg', 'custom']. Default installmode: custom")
     parser.addoption("--installpath", action="store", help="Set installpath for installed syslog-ng. Used when installmode is: custom. Example path: '/home/user/syslog-ng/installdir/'")
-    parser.addoption("--syslogngversion", action="store", default="3.13", help="Used syslog-ng version in configurations. Default: 3.13")
+    parser.addoption("--syslogngversion", action="store", help="Used syslog-ng version in configurations")
 
 
 @pytest.fixture
@@ -22,6 +25,14 @@ def installpath(request):
 
 @pytest.fixture
 def syslogngversion(request):
+    if not request.config.getoption("--syslogngversion") :
+        syslog_ng_binary_path = os.path.join("%s" % installpath(request),"sbin/syslog-ng")
+        command = "%s -V" % (syslog_ng_binary_path)
+        command_args = shlex.split(command)
+        syslog_ng_version = subprocess.check_output(command_args).decode("utf-8").splitlines()
+        for line in syslog_ng_version:
+            if "Config version" in line:
+                return line.split()[2]
     return request.config.getoption("--syslogngversion")
 
 
