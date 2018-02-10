2995a3a884680fa0bcd03688d5283853cc907934
Andras Mitzki <andras.mitzki@balabit.com>
2018-02-10 22:27:58 +0100

    ReviewNoteFix: Functional testcase updates
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- tests/pytest_framework/functional_tests/test_file_source_generated_single_message.py
+++ tests/pytest_framework/functional_tests/test_file_source_generated_single_message.py
@@ -4,7 +4,9 @@ def test_file_source_generated_message(tc):
     cfg.add_global_options({"stats_level": 3})
     cfg_fs = cfg.get_filesource("input")
     cfg_fd = cfg.get_filedestination("output")
-    cfg.create_logpath(sources=cfg_fs, destinations=cfg_fd)
+    cfg_fd2 = cfg.get_filedestination("output2")
+    cfg_fd3 = cfg.get_filedestination("output3")
+    cfg.create_logpath(sources=cfg_fs, destinations=[cfg_fd, cfg_fd2, cfg_fd3])
 
     # generate message + send
     message_counter = 1
@@ -22,5 +24,15 @@ def test_file_source_generated_message(tc):
     expected_output_message = cfg_fd.get_expected_output_message(message_parts={"program": "MYPROGRAM"}, expected_message_counter=message_counter)
 
     assert output_message == expected_output_message
-    assert cfg_fd.get_query() == {'memory_usage': 0, 'written': 1, 'processed': 1, 'dropped': 0, 'queued': 0}
-    assert cfg_fs.get_query()["processed"] == 1
+    assert cfg_fd.get_query() == {'memory_usage': 0, 'written': message_counter, 'processed': message_counter, 'dropped': 0, 'queued': 0}
+    assert cfg_fs.get_query()["processed"] == message_counter
+
+    slng.syslog_ng_ctl.check_stats_and_query_counters(cfg.syslog_ng_config, destination_counter_values={
+        "processed": message_counter,
+        "written": message_counter,
+        "dropped": 0,
+        "queued": 0,
+        "memory_usage": 0
+    }, source_counter_values = {
+        "processed": message_counter
+    })
