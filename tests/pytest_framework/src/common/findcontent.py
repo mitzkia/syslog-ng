cb789d41bc3ca03fcbbe21f889c72b4c1d38ae11
Andras Mitzki <andras.mitzki@balabit.com>
2018-02-10 12:46:40 +0100

    ReviewNoteFix: Findcontent
     - refactor find_regexp_in_content()
    
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- tests/pytest_framework/src/common/findcontent.py
+++ tests/pytest_framework/src/common/findcontent.py
@@ -1,11 +1,5 @@
 import re
 
 def find_regexp_in_content(regexp, content, expected_counter=1):
-    found = False
-    counter = 0
     regexp_pattern = re.compile(regexp)
-    for message in content.splitlines():
-        if regexp_pattern.match(message) is not None:
-            counter += 1
-            found = True
-    return found and (counter == expected_counter)
+    return expected_counter == len(list(filter(regexp_pattern.match, content.splitlines())))
