9b4202a4683ab9267e752119741ad1b6bad08953
Andras Mitzki <andras.mitzki@balabit.com>
2018-02-11 07:24:22 +0100

    ReviewNoteFix: Highlight "content receive"/"content write" in console log
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- tests/pytest_framework/src/driverio/fileio.py
+++ tests/pytest_framework/src/driverio/fileio.py
@@ -1,4 +1,5 @@
 from src.driverio.filemanager import FileManagerWaitForEvent
+from colorama import Fore, Back, Style
 
 
 class FileIO(object):
@@ -10,14 +11,24 @@ class FileIO(object):
         file_manager = FileManagerWaitForEvent(self.logger_factory, file_path)
         file_manager.wait_for_creation()
         file_manager.wait_until_number_of_lines(expected_lines=expected_message_counter)
-        return file_manager.get_content()
+        file_content = file_manager.get_content()
+        self.logger.info(Fore.BLACK + Back.YELLOW +
+                        "Content received:" + Style.RESET_ALL +
+                        "\n>>>From path:[%s]\n" % file_path +
+                        "\n>>>Content:[%s]" % file_content
+                        )
+        return file_content
 
     def write(self, file_path, content, append=True):
         if append:
             open_mode = "a+"
         else:
             open_mode = "w"
-        self.logger.info("SUBSTEP Content write\n>>>ContentIn:[%s]\n>>>to path:[%s]" % (content, file_path))
+        self.logger.info(Fore.BLACK + Back.YELLOW +
+                         "Content written:" + Style.RESET_ALL +
+                         "\n>>>From path:[%s]\n" % file_path +
+                         "\n>>>Content:[%s]" % content
+                         )
         with open(file_path, open_mode) as file_object:
             file_object.write(self.normalize_line_endings(content))
 
