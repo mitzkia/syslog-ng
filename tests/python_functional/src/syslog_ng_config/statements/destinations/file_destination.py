11e874c48de4e4459c140dbb217101ef0e4ef2de
Andras Mitzki <andras.mitzki@balabit.com>
2019-06-28 10:13:07 +0200

    LightSyslogNgConfig: Fixup start using stats and query from config
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- tests/python_functional/src/syslog_ng_config/statements/destinations/file_destination.py
+++ tests/python_functional/src/syslog_ng_config/statements/destinations/file_destination.py
@@ -27,6 +27,64 @@ from src.common.random_id import get_unique_id
 from src.driver_io.file.file_io import FileIO
 from src.message_reader.single_line_parser import SingleLineParser
 from src.syslog_ng_config.statements.destinations.destination_reader import DestinationReader
+from src.syslog_ng_ctl.syslog_ng_ctl import SyslogNgCtl
+
+
+class DriverStatsHandler(object):
+    def __init__(self, group_type, driver_name):
+        if group_type == "destination":
+            statement_short_name = "dst"
+        elif group_type == "source":
+            statement_short_name = "src"
+
+        self.component = "{}.{}".format(statement_short_name, driver_name)
+        self.syslog_ng_ctl = SyslogNgCtl(tc_parameters.INSTANCE_PATH)
+
+    def build_query_pattern(self):
+        return self.build_pattern(delimiter=".")
+
+    def build_stats_pattern(self):
+        return self.build_pattern(delimiter=";")
+
+    def build_pattern(self, delimiter):
+        return "{}{}".format(
+            self.component,
+            delimiter,
+        )
+
+    def parse_result(self, result, result_type):
+        statistical_elements = ["memory_usage", "written", "processed", "dropped", "queued", "stamp"]
+        parsed_output = {}
+        for stat_element in statistical_elements:
+            for line in result:
+                if stat_element in line:
+                    if result_type == "query":
+                        parsed_output.update({stat_element: int(line.split(".")[-1].split("=")[-1])})
+                    elif result_type == "stats":
+                        parsed_output.update({stat_element: int(line.split(".")[-1].split(";")[-1])})
+                    else:
+                        raise Exception("Unknown result_type: {}".format(result_type))
+        return parsed_output
+
+    def filter_stats_result(self, stats_result, stats_pattern):
+        filtered_list = []
+        for stats_line in stats_result:
+            if stats_pattern in stats_line:
+                filtered_list.append(stats_line)
+        return filtered_list
+
+    def get_query(self):
+        query_pattern = self.build_query_pattern()
+        query_result = self.syslog_ng_ctl.query(pattern="*{}*".format(query_pattern))['stdout'].splitlines()
+
+        return self.parse_result(result=query_result, result_type="query")
+
+    def get_stats(self):
+        stats_pattern = self.build_stats_pattern()
+        stats_result = self.syslog_ng_ctl.stats()['stdout'].splitlines()
+
+        filtered_stats_result = self.filter_stats_result(stats_result, stats_pattern)
+        return self.parse_result(result=filtered_stats_result, result_type="stats")
 
 
 class FileDestination(object):
@@ -42,6 +100,7 @@ class FileDestination(object):
             else:
                 self.positional_option = Path(tc_parameters.WORKING_DIR, file_name)
         self.destination_reader = DestinationReader(FileIO, SingleLineParser)
+        self.stats_handler = DriverStatsHandler(group_type=self.group_type, driver_name=self.driver_name)
 
     def get_path(self):
         return self.positional_option
@@ -55,3 +114,9 @@ class FileDestination(object):
 
     def read_logs(self, counter):
         return self.destination_reader.read_logs(self.get_path(), counter=counter)
+
+    def get_stats(self):
+        return self.stats_handler.get_stats()
+
+    def get_query(self):
+        return self.stats_handler.get_query()
