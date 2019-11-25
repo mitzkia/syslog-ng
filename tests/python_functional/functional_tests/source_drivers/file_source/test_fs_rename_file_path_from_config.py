9dd036e3f9fbd7fd4361e7a165d8a5d702c23df0
Andras Mitzki <andras.mitzki@balabit.com>
2019-11-25 05:27:02 +0100

    LightFunctionalTestFile: Extend tests when path was renamed from config
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

---
+++ tests/python_functional/functional_tests/source_drivers/file_source/test_fs_rename_file_path_from_config.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#############################################################################
+# Copyright (c) 2015-2019 Balabit
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+from pathlib2 import Path
+
+import src.testcase_parameters.testcase_parameters as tc_parameters
+
+
+def test_fs_rename_file_path_from_config(config, syslog_ng):
+    config.update_global_options(keep_hostname="yes")
+    file_source = config.create_file_source(file_name="input.log")
+    file_destination = config.create_file_destination()
+    config.create_logpath(statements=[file_source, file_destination])
+
+    file_source.write_log("custom message 1\n")
+    syslog_ng.start(config)
+    assert "custom message 1" in file_destination.read_log()
+
+    new_input_path = Path(tc_parameters.WORKING_DIR, "input2.log")
+    file_source.set_path(str(new_input_path))
+    assert not new_input_path.exists()
+
+    file_source.write_log("custom message 2\n")
+    assert new_input_path.exists()
+    syslog_ng.reload(config)
+
+    assert "custom message 2" in file_destination.read_log()
