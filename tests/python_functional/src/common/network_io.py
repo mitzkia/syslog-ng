72d91ad5304b15c2f2858504b25cde9a27877df1
László Várady <laszlo.varady@protonmail.com>
2021-05-31 20:05:13 +0200

    light-common: add TCPServer and UnixStreamServer
    SingleConnectionStreamServer is an abstract class that implements some IO
    functionality on stream-based servers, using only one connection.
    
    The following public methods are available:
    - start(), stop(), close_client()
    - readexactly(n), readuntil(separator), readline(), write(data)
    
    SingleConnectionTCPServer is a basic TCP/TLS server implementation based
    on SingleConnectionStreamServer.
    
    UnixStreamServer is the same, but it operates on Unix domain stream
    sockets.
    
    Note: This is a low-level network layer, it operates on bytes objects,
    not on strings or messages.
    
    Note: Using async and asyncio was not an essential decision, blocking on
    each async call has no advantage over a blocking threaded
    implementation. async is used here because the streaming and thread
    synchronization APIs of asyncio are elegant and easier to understand in
    my opinion.
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>

--- tests/python_functional/src/common/network_io.py
+++ tests/python_functional/src/common/network_io.py
@@ -19,10 +19,15 @@
 # COPYING for details.
 #
 #############################################################################
-
 import asyncio
 import concurrent
+import socket
 import threading
+from abc import ABC
+from abc import abstractmethod
+
+from src.common.blocking import DEFAULT_TIMEOUT
+
 
 class BackgroundEventLoop(object):
     """Singleton event loop running in the background"""
@@ -56,3 +61,119 @@ class BackgroundEventLoop(object):
 
     def stop(self):
         self.loop.call_soon_threadsafe(self.loop.stop)
+
+
+class SingleConnectionStreamServer(ABC):
+    def __init__(self):
+        self._event_loop = BackgroundEventLoop()
+        self._server = None
+        self._client = self._event_loop.run_async(self._create_client()).result()
+
+    def start(self):
+        server_res = self._event_loop.run_async(self._start_server())
+        self._server = server_res.result()
+
+    def stop(self):
+        self._event_loop.run_async(self._stop_server()).result()
+        self.close_client()
+
+    def close_client(self):
+        """After a client connection is accepted, new connections will be rejected until this method is called"""
+        self._event_loop.run_async(self._client.close()).result()
+
+    def readexactly(self, n, timeout=DEFAULT_TIMEOUT):
+        return self._event_loop.wait_async_result(self._readexactly(n), timeout=timeout)
+
+    def readuntil(self, separator=b'\n', timeout=DEFAULT_TIMEOUT):
+        return self._event_loop.wait_async_result(self._readuntil(separator), timeout=timeout)
+
+    def readline(self, timeout=DEFAULT_TIMEOUT):
+        return self._event_loop.wait_async_result(self._readline(), timeout=timeout)
+
+    def write(self, data, timeout=DEFAULT_TIMEOUT):
+        self._event_loop.wait_async_result(self._write(data), timeout=timeout)
+
+    def get_event_loop(self):
+        return self._event_loop
+
+    async def _create_client(self):
+        return self.Client()
+
+    @abstractmethod
+    async def _start_server(self):
+        pass
+
+    async def _stop_server(self):
+        if self._server is not None:
+            self._server.close()
+            await self._server.wait_closed()
+
+    async def _client_accepted_cb(self, reader, writer):
+        self._client.accept_connection(reader, writer)
+
+    async def _readexactly(self, n):
+        await self._client.wait_for_client_connection()
+        return await self._client._reader.readexactly(n)
+
+    async def _readuntil(self, separator):
+        await self._client.wait_for_client_connection()
+        return await self._client._reader.readuntil(separator)
+
+    async def _readline(self):
+        await self._client.wait_for_client_connection()
+        return await self._client._reader.readline()
+
+    async def _write(self, data):
+        await self._client.wait_for_client_connection()
+        self._client._writer.write(data)
+        await self._client._writer.drain()
+
+    class Client(object):
+        def __init__(self):
+            self._connection_accepted = asyncio.Event()
+            self._reader = None
+            self._writer = None
+
+        def accept_connection(self, reader, writer):
+            if self._connection_accepted.is_set():
+                writer.close()
+                return
+            self._reader = reader
+            self._writer = writer
+            self._connection_accepted.set()
+
+        async def wait_for_client_connection(self):
+            await self._connection_accepted.wait()
+
+        async def close(self):
+            if not self._connection_accepted.is_set():
+                return
+            self._writer.close()
+            await self._writer.wait_closed()
+            self._connection_accepted.clear()
+
+
+class SingleConnectionTCPServer(SingleConnectionStreamServer):
+    def __init__(self, port, host=None, ip_protocol_version=socket.AF_INET, ssl=None):
+        super(SingleConnectionTCPServer, self).__init__()
+        self._host = host
+        self._port = port
+        self._family = ip_protocol_version
+        self._ssl = ssl
+
+    async def _start_server(self):
+        server = await asyncio.start_server(self._client_accepted_cb, self._host, self._port, family=self._family, ssl=self._ssl)
+        await server.start_serving()
+        return server
+
+
+class SingleConnectionUnixStreamServer(SingleConnectionStreamServer):
+    def __init__(self, path, ssl=None):
+        super(SingleConnectionUnixStreamServer, self).__init__()
+        self._path = path
+        self._ssl = ssl
+
+    async def _start_server(self):
+        server = await asyncio.start_unix_server(self._client_accepted_cb, self._path, ssl=self._ssl)
+        await server.start_serving()
+        return server
